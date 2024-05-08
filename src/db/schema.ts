8156c28bb28a6bfe2877283e1245a6b0edaae03d
Pfannkuchen <hello@gougoule.ch>
2024-05-08 17:20:37 +0200

    add notes and conversations table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, boolean, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, boolean, timestamp, integer } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: serial('id'),
@@ -8,4 +8,22 @@ export const user = pgTable('user', {
 	emailVerified: boolean('email_verified'),
 	emailVerificationCode: text('email_verification_code'),
 	lastEmailVerificationCodeSentAt: timestamp('last_email_verification_code_sent_at')
+});
+
+export const note = pgTable('note', {
+	id: serial('id'),
+	children: text('children'),
+	parent: text('parent'),
+	title: text('title'),
+	body: text('body'),
+	userId: integer('user_id'),
+	createdAt: timestamp('created_at'),
+	updatedAt: timestamp('updated_at')
+});
+
+export const conversation = pgTable('conversation', {
+	id: serial('id'),
+	userId: integer('user_id'),
+	title: text('title'),
+	body: text('body')
 });
