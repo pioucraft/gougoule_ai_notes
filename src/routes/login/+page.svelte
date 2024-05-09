341610c97b54f65b4c728252bc35c719e22ccecd
Pfannkuchen <hello@gougoule.ch>
2024-05-09 09:38:49 +0200

    advancing in the log in page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,31 @@
 <script>
 	export let data;
+
+	/*
+	{#if data.signUpAllowed}
+		<p>Sign Up</p>
+	{/if}
+	*/
 </script>
 
 <div id="wrapper">
 	<div id="wrapper-loginBox">
-		{#if data.signUpAllowed}
-			<p>Sign Up</p>
-		{/if}
+		<h2>Log in to continue :</h2>
+		<div id="wrapper-loginBox-email">
+			<p>Email :</p>
+			<input type="email">
+		</div>
+
+		<div id="wrapper-loginBox-password">
+			<p>Password :</p>
+			<input type="password">
+		</div>
+		
+		<button id="wrapper-loginBox-button">Log in
+			{#if data.signUpAllowed}
+			or sign up
+			{/if}
+		</button>
 	</div>
 </div>
 
