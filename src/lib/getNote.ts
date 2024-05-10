10f16a783dccd8548774da346146157ccc5d2872
Pfannkuchen <hello@gougoule.ch>
2024-05-10 21:19:23 +0200

    advancing in things and add todos

--- src/lib/getNote.ts
+++ src/lib/getNote.ts
@@ -1,15 +1,15 @@
-import { and, eq, isNull } from "drizzle-orm";
+import { and, eq, isNull, or } from "drizzle-orm";
 import { db } from "../db/db";
-import { note, type noteType } from "../db/schema";
+import { note, user, type noteType } from "../db/schema";
 import { getUserId } from "./getUserId";
 
-export async function getNote(noteId: number | null, token: string): Promise<noteType | string | noteType[]> {
+export async function getNote(noteId: number | null, token: string): Promise<string | noteType[]> {
     const userId = await getUserId(token);
     if(userId == false) return "token invalid"
     if(noteId == null) {
         return (await db.select().from(note).where(and(isNull(note.parent), eq(note.userId, userId))));
     }
-    let content = await db.select().from(note).where(and(eq(note.id, noteId), eq(note.userId, userId)));
-    if(content.length) return content[0]
+    let content = await db.select().from(note).where(or(and(eq(note.id, noteId), eq(note.userId, userId)), and(eq(note.userId, userId), eq(note.parent, noteId))));
+    if(content.length) return content
     else return "note note found"
 }
--- src/routes/[file]/+page.svelte
+++ src/routes/[file]/+page.svelte
@@ -1,5 +1,23 @@
-<script>
-    export let data
-</script>
+<div id="wrapper">
+    <div id="wrapper-sidebar">
+        {JSON.stringify(data)}
+    </div>
+</div>
 
-<p>{JSON.stringify(data)}</p>
+<script lang="ts">
+	import { toasts } from 'svelte-simpletoast';
+
+    export let data;
+    if(data.content == null) {
+        toasts.error("Error", "Error", 3000, true);
+    }
+
+    //add thhis inside the html code
+    else if(Array.isArray(data.content)) {
+        // show folders and conversations on the sidebar and back arrow (if parent not null)
+    }
+    else {
+        // show backArrow
+    }
+    
+</script>
